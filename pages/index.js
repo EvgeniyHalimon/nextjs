2ea9bb895b42b39ea34634c46c9d14f148d005a6
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-01-10 15:27:35 +0200

    added sass and styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Heading from "../components/Heading"
+import styles from "../styles/Home.module.scss"
 
 const Home = () => (
-  <Heading text="Got the life"/>
+  <div className={styles.wrapper}>
+    <Heading text="Got the life"/>
+  </div>
 )
 
 export default Home
